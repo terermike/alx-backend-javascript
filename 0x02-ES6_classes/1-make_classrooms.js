9f19ec55b787e74af1332e432489964e9d85c5f0
Terer <terermike3@gmail.com>
2023-04-21 23:52:48 +0000

    1-make_classrooms.js

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,9 +1,7 @@
 import ClassRoom from './0-classroom.js';
 
 export default function initializeRooms() {
- return [
-  new ClassRoom(19),
-  new ClassRoom(20),
-  new ClassRoom(34),
-  ];	
+  const roomSizes = [19, 20, 34];
+  return roomSizes.map(size => new ClassRoom(size));
 }
+
