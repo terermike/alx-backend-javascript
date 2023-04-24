26a65265d98c3a3a6ce58d18490aa446d8b3589c
Terer <terermike3@gmail.com>
2023-04-24 22:21:30 +0000

    1-get_list_student_ids.js

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,7 +1,6 @@
 export default function getListStudentIds(arr) {
-  if (!Array.isArray(arr)) {
-    return [];
-  } else {
+  if (Array.isArray(arr)) {
     return arr.map((item) => item.id);
-  }
+  } 
+    return [];
 }
