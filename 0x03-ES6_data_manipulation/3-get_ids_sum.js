c4aeb17e71613cf9172956890a4e3dcb7e4800bb
Terer <terermike3@gmail.com>
2023-04-24 23:30:04 +0000

    3-get_ids_sum.js

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,5 +1,5 @@
 import getListStudents from './0-get_list_students';
 
 export default function getStudentIdsSum(list = getListStudents()) {
-  return list.reduce((current, next) => current + next.id, 0);	
+  return list.reduce((curr, next) => curr + next.id, 0);
 }
