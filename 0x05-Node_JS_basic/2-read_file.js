021d6b0de1bc0f52557827d33e19650056aaed1b
Terer <terermike3@gmail.com>
2023-06-13 18:51:01 +0300

    2-read_file.js

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,39 +1,74 @@
 const fs = require('fs');
 
 function countStudents(fileName) {
-  const studentsByField = {};
-  const studentsCountByField = {};
-  let totalStudents = 0;
-
+  const students = {};
+  const fields = {};
+  let length = 0;
   try {
-    const fileContent = fs.readFileSync(fileName, 'utf-8');
-    const lines = fileContent.split('\n');
-
+    const content = fs.readFileSync(fileName, 'utf-8');
+    const lines = content.toString().split('\n');
     for (let i = 0; i < lines.length; i += 1) {
-      const line = lines[i].trim();
-      if (line) {
-        const [firstName, lastName, age, field] = line.split(',');
-
-        if (!studentsByField[field]) {
-          studentsByField[field] = [];
-          studentsCountByField[field] = 0;
+      if (lines[i]) {
+        length += 1;
+        const field = lines[i].toString().split(',');
+        if (Object.prototype.hasOwnProperty.call(students, field[3])) {
+          students[field[3]].push(field[0]);
+        } else {
+          students[field[3]] = [field[0]];
         }
-
-        studentsByField[field].push(firstName);
-        studentsCountByField[field] += 1;
-        totalStudents += 1;
+        if (Object.prototype.hasOwnProperty.call(fields, field[3])) {
+          fields[field[3]] += 1;
+        } else {
+          fields[field[3]] = 1;
+        }
       }
     }
-
-    console.log(`Number of students: ${totalStudents}`);
-
-    for (const [field, count] of Object.entries(studentsCountByField)) {
-      if (field !== 'field') {
-        console.log(`Number of students in ${field}: ${count}. List: ${studentsByField[field].join(', ')}`);
+    const l = length - 1;
+    console.log(`Number of students: ${l}`);
+    for (const [key, value] of Object.entries(fields)) {
+      if (key !== 'field') {
+        console.log(`Number of students in ${key}: ${value}. List: ${students[key].join(', ')}`);
       }
     }
   } catch (error) {
-    throw new Error('Cannot load the database');
+    throw Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;const fs = require('fs');
+
+function countStudents(fileName) {
+  const students = {};
+  const fields = {};
+  let length = 0;
+  try {
+    const content = fs.readFileSync(fileName, 'utf-8');
+    const lines = content.toString().split('\n');
+    for (let i = 0; i < lines.length; i += 1) {
+      if (lines[i]) {
+        length += 1;
+        const field = lines[i].toString().split(',');
+        if (Object.prototype.hasOwnProperty.call(students, field[3])) {
+          students[field[3]].push(field[0]);
+        } else {
+          students[field[3]] = [field[0]];
+        }
+        if (Object.prototype.hasOwnProperty.call(fields, field[3])) {
+          fields[field[3]] += 1;
+        } else {
+          fields[field[3]] = 1;
+        }
+      }
+    }
+    const l = length - 1;
+    console.log(`Number of students: ${l}`);
+    for (const [key, value] of Object.entries(fields)) {
+      if (key !== 'field') {
+        console.log(`Number of students in ${key}: ${value}. List: ${students[key].join(', ')}`);
+      }
+    }
+  } catch (error) {
+    throw Error('Cannot load the database');
   }
 }
 
